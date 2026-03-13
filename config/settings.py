a5f19566a17cfbecb6683d41acdf3ac5492c74b0
Teken <timu.rev006@gamil.com>
2026-03-13 10:51:06 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.blog.apps.BlogConfig',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
