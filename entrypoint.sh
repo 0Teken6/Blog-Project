af34ba9e1f484d63e6b6ec115361de92a20c877d
Teken <timu.rev006@gamil.com>
2026-03-24 14:18:09 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
